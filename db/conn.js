4d293071e754ab6eccc3d4626a2ba3a78a081bb6
Saif Ahmad <76485570+Saifahmad-svg@users.noreply.github.com>
2022-03-16 17:37:52 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURL = `mongodb+srv://saif_svg:9279537277@cluster0.pom7b.mongodb.net/Poll-app?retryWrites=true&w=majority`
+
 // const mongoURL = "mongodb://localhost:27017/poll"
 
 mongoose.connect(mongoURL,{
